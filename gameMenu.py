2b188f151111ef9e9a001f6f1bc0133b47a6209f
dean36963 <dean.birch0@gmail.com>
2014-01-02 14:03:14 +0000

    Adding settings reader and allowing it to define sounds and use of joystick

--- gameMenu.py
+++ gameMenu.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python2
 import joystick
 import pygame
+from settings import Settings
 import csv
 import menuEntry
 import time
@@ -16,23 +17,28 @@ class GameMenu():
 	def __init__(self):
 		print "Starting Menu"
 		self.fail=False
-		self.joy=joystick.Joystick()
-		if (self.joy.errors!=[]):
-			for error in self.joy.errors:
-				print error
-				self.use_joystick=False
-		else:
-			self.use_joystick=True
-			
+		#Should read settings first
 		self.get_settings()
+
+		if self.settings.get('useJoystick') not in ['N','No','False']:
+			self.joy=joystick.Joystick()
+			if (self.joy.errors!=[]):
+				for error in self.joy.errors:
+					print error
+					self.use_joystick=False
+			else:
+				self.use_joystick=True
+		else:
+			print "Ignoring any joystick due useJoystick in properties"
+			self.use_joystick=False
+			
 		self.menuEntries = menuEntry.MenuEntry(self.num_entries_x,self.num_entries_y)
 		self.menuEntries.settings(self.borders,self.gap_x,self.gap_y,self.menu_size_x,self.menu_size_y)
 		self.readMenuFile()
 		self.menuOutOfDate=False
 		self.menuEntries.defined()
-		if (self.use_sound):
+		if (self.settings.get('use_sound')):
 			self.setupSounds()
-		#TODO make more robust - not uploading a backgorund
 		if (os.path.exists(self.image_dir+"/background.jpg")):
 			self.background = pygame.image.load("resources/images/background.jpg")
 			self.background = pygame.transform.scale(self.background,(self.screen_size_x,self.screen_size_y))
@@ -59,17 +65,18 @@ class GameMenu():
 		self.clock=pygame.time.Clock()
 		
 	def get_settings(self):
+		self.settings = Settings('local.properties')
+		self.settings.print_data()
 		#Replace with reading settings file
 		print "Using Default Settings"
-		self.use_sound = True
 		self.sound_dir = "resources/sounds"
 		self.image_dir = "resources/images"
-		self.screen_size_x = 1024
-		self.screen_size_y = 600
+		#self.screen_size_x = 1024
+		#self.screen_size_y = 600
 		#self.screen_size_x = 640
 		#self.screen_size_y = 480
-		#self.screen_size_x = 1920
-		#self.screen_size_y = 1080
+		self.screen_size_x = 1920
+		self.screen_size_y = 1080
 		
 		self.frames_per_second = 5
 		self.tick = 1.0 / self.frames_per_second
@@ -77,7 +84,8 @@ class GameMenu():
 		self.num_entries_x = 4
 		self.num_entries_y = 3
 		self.num_pages = 2
-		self.menuFile = '/srv/gameEditor/gameMenu.csv'
+		#self.menuFile = '/srv/gameEditor/gameMenu.csv'
+		self.menuFile = 'gameMenu.csv'
 		
 		#Gap between menu entries
 		self.gap_x = 20
@@ -113,37 +121,37 @@ class GameMenu():
 				self.keyboard_loop()
 				continue
 			if (joy_input[1]==1):
-				if (self.use_sound):
+				if (self.settings.get('use_sound')):
 					self.sounds["exit"].play()
 					time.sleep(0.5)
 				loop=False
 			if (joy_input[0]==1):
-				if (self.use_sound):
+				if (self.settings.get('use_sound')):
 					self.sounds["select"].play()
 				self.launch()
 			if (joy_input[5]<-0.5):
 				self.menuEntries.move_left()
-				if (self.use_sound):
+				if (self.settings.get('use_sound')):
 					self.sounds["move"].play()
 			if (joy_input[5]>0.5):
 				self.menuEntries.move_right()
-				if (self.use_sound):
+				if (self.settings.get('use_sound')):
 					self.sounds["move"].play()
 			if (joy_input[6]<-0.5):
 				self.menuEntries.move_up()
-				if (self.use_sound):
+				if (self.settings.get('use_sound')):
 					self.sounds["move"].play()
 			if (joy_input[6]>0.5):
 				self.menuEntries.move_down()
-				if (self.use_sound):
+				if (self.settings.get('use_sound')):
 					self.sounds["move"].play()
 			if (joy_input[2]==1):
 				self.menuEntries.prev_page()
-				if (self.use_sound):
+				if (self.settings.get('use_sound')):
 					self.sounds["move"].play()
 			if (joy_input[3]==1):
 				self.menuEntries.next_page()
-				if (self.use_sound):
+				if (self.settings.get('use_sound')):
 					self.sounds["move"].play()
 		self.quit()
 			
@@ -161,12 +169,12 @@ class GameMenu():
 			loop=True
 			while (loop):
 				x=pygame.key.get_pressed()
-				print(type(x))
+				#print(type(x))
 				if ( self.joy.get_input()[4]):
 					self.menuEntries.stop()
 					loop=False
 				time.sleep(self.tick)
-		if (self.use_sound):
+		if (self.settings.get('use_sound')):
 			self.sounds["exit"].play()
 		pygame.event.clear()
 		time.sleep(1)
@@ -201,43 +209,43 @@ class GameMenu():
 		else:
 			print "Could not find sound files: move.ogg select.ogg exit.ogg"
 			print "To enable sound, add them to resources/sounds/"
-			self.use_sound = False
+			self.settings.set('use_sound',None)
 
 		
 	def keyboard_loop(self):
 		for event in pygame.event.get():
 			if event.type == KEYDOWN and event.key == K_ESCAPE:
-				if (self.use_sound):
+				if (self.settings.get('use_sound')):
 					self.sounds["exit"].play()
 					time.sleep(0.5)
 				self.quit()
 			if event.type == KEYDOWN and event.key == K_RETURN:
-				if (self.use_sound):
+				if (self.settings.get('use_sound')):
 					self.sounds["select"].play()
 				self.launch()
 			if event.type == KEYDOWN and event.key == K_LEFT:
 				self.menuEntries.move_left()
-				if (self.use_sound):
+				if (self.settings.get('use_sound')):
 					self.sounds["move"].play()
 			if event.type == KEYDOWN and event.key == K_RIGHT:
 				self.menuEntries.move_right()
-				if (self.use_sound):
+				if (self.settings.get('use_sound')):
 					self.sounds["move"].play()
 			if event.type == KEYDOWN and event.key == K_UP:
 				self.menuEntries.move_up()
-				if (self.use_sound):
+				if (self.settings.get('use_sound')):
 					self.sounds["move"].play()
 			if event.type == KEYDOWN and event.key == K_DOWN:
 				self.menuEntries.move_down()
-				if (self.use_sound):
+				if (self.settings.get('use_sound')):
 					self.sounds["move"].play()
 			if event.type == KEYDOWN and event.key == K_PAGEDOWN:
 				self.menuEntries.prev_page()
-				if (self.use_sound):
+				if (self.settings.get('use_sound')):
 					self.sounds["move"].play()
 			if event.type == KEYDOWN and event.key == K_PAGEUP:
 				self.menuEntries.next_page()
-				if (self.use_sound):
+				if (self.settings.get('use_sound')):
 					self.sounds["move"].play()
 	def rereadMenuFile(self):
 		self.menuEntries = menuEntry.MenuEntry(self.num_entries_x,self.num_entries_y)
---
+++ settings.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python2
+
+class Settings():
+	def __init__(self,file_path=None):
+		self.__data__ = dict()
+		if file_path is not None:
+			self.read_properties_file(file_path)
+	def read_properties_file(self,file_path):
+		try:
+			file=open(file_path)
+			for line in file:
+				self.__parse_line__(line)
+			return True
+		except Exception as e:
+			print "Failed to read",file_path
+			print e
+			return False
+			
+	def __parse_line__(self,line):
+		if line.strip().startswith('#'):
+			pass
+		if '=' in line:
+			property_name,property_value = line.split('=')
+			property_name = property_name.strip()
+			property_value = property_value.strip()
+			if property_name in self.__data__.keys():
+				print "Property \""+property_name+"\" appears more than once, taking latest value."
+			self.__data__[property_name]=property_value
+
+	def print_data(self):
+		print self.__data__
+
+	def get(self,property_name):
+		try:
+			prop = self.__data__[property_name]
+		except:
+			prop = None
+		return prop
+
+	def set(self,property_name,value):
+		self.__data__[property_name] = value
+
+if __name__ == '__main__':
+	print "Running this shit"
+	s = Settings('test.properties')
+	s.print_data()
